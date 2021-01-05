1631c24bbc1ea90ee97e3f7981330025774ff3d3
Stephen Johnson <stephen.a.johnson@me.com>
2021-01-05 14:18:43 +0000

    Functions calling other Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -57,6 +57,7 @@ console.log(age1, age2);
 
 */
 
+/*
 // Arrow Function
 const calcAge3 = birthYear => 2037 - birthYear;
 
@@ -72,3 +73,39 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 
 console.log(yearsUntilRetirement(1991, 'Stephen'));
 console.log(yearsUntilRetirement(1980, 'Bob'));
+*/
+
+/*
+// Function calling other function
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+    return juice;
+}
+console.log(fruitProcessor(2, 3));
+*/
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+        return retirement;
+    } else {
+        return -1;
+    }
+
+}
+console.log(yearsUntilRetirement(1991, 'Stephen'));
+console.log(yearsUntilRetirement(1970, 'Mike'));
+
